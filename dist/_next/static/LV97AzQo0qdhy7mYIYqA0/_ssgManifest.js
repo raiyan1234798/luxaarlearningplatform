6796968f4adf331d0ae6b8dd7961ded7e381441d
Raiyan <rayan@Rayans-MacBook-Air.local>
2026-02-19 10:38:18 +0530

    Configure firebase.json for static hosting (dist)

---
+++ dist/_next/static/LV97AzQo0qdhy7mYIYqA0/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fdashboard\u002Fadmin\u002Fcourses\u002F[courseId]\u002Fedit","\u002Fdashboard\u002Fcourses\u002F[courseId]","\u002Fdashboard\u002Fcourses\u002F[courseId]\u002Flearn"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
